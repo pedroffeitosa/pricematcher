23b6423d3744123afef340279cc8caf2e92ea843
Pedro França <jppfeitosa@gmail.com>
2023-10-25 18:50:17 -0300

    fix: carcteres indisponíveis

--- prolog/src/menu.pl
+++ prolog/src/menu.pl
@@ -3,12 +3,12 @@
 menu_principal :-
     writeln('Bem-vindo ao PriceMatcher - Sistema de Gerenciamento de Desenvolvedores e Projetos'),
     repeat,
-    writeln('Escolha uma opção:'),
+    writeln('Escolha uma opcap:'),
     writeln('1. Adicionar Desenvolvedor'),
-    writeln('2. Adicionar Especialização a um Desenvolvedor'),
-    writeln('3. Adicionar Experiência a um Desenvolvedor'),
+    writeln('2. Adicionar Especializacao a um Desenvolvedor'),
+    writeln('3. Adicionar Experiencia a um Desenvolvedor'),
     writeln('4. Adicionar Projeto'),
-    writeln('5. Listar Projetos (Histórico de Projetos)'),
+    writeln('5. Listar Projetos (Historico de Projetos)'),
     writeln('6. Sair'),
     read(Opcao),
     executar_opcao(Opcao),
@@ -20,7 +20,7 @@ executar_opcao(1) :-
     console_adc_desenv,
     menu_principal.
 executar_opcao(2) :-
-    writeln('Adicionar Especialização a um Desenvolvedor'),
+    writeln('Adicionar Especializao a um Desenvolvedor'),
     console_adc_espec,
     menu_principal.
 executar_opcao(3) :-
@@ -32,7 +32,7 @@ executar_opcao(4) :-
     console_adc_projeto,
     menu_principal.
 executar_opcao(5) :-
-    writeln('Listar Projetos (Histórico de Projetos)'),
+    writeln('Listar Projetos (Historico de Projetos)'),
     listar_projetos, % Você deve implementar a lógica para listar os projetos aqui
     menu_principal.
 executar_opcao(6).
