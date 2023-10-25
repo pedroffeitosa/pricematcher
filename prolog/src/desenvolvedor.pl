9ff7cab01eff4f6b2b25c087ff9db324d07c7e5c
Lucas de Oliveira <lucas.oliveira.silva@ccc.ufcg.edu.br>
2023-10-25 17:02:46 -0300

    criado arquivo console.pl para ter as clausulas que recebem os dados do terminal

--- prolog/src/desenvolvedor.pl
+++ prolog/src/desenvolvedor.pl
@@ -1,2 +1,4 @@
 :- dynamic desenvolvedor/5.
 
+desenvolvedor(nome("Lucas de Oliveira"), especializacoes(["Python"]), experiencia([["UFCG", "10/2016", "12/2025"]]), valor_hora(16.93), multiplicador(2.42)).
+
--- prolog/src/projeto.pl
+++ prolog/src/projeto.pl
@@ -1,2 +1,4 @@
 :- dynamic projeto/5.
 
+projeto(descricao("projeto 1"), prazo("10/10/2024"), complexidade("Dificil"), requisitos(["Python", "java", "sql"]), horas(240)).
+
